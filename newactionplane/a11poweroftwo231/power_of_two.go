87884e23d3f01a36d3c07cc8b020009da2bf4e7a
mark <mark.h.tang@gmail.com>
2025-04-19 08:10:44 +0800

    feat: power-of-two https://leetcode.cn/problems/power-of-two/description/?envType=study-plan-v2&envId=primers-list

---
+++ newactionplane/a11poweroftwo231/power_of_two.go
@@ -0,0 +1,15 @@
+package a11poweroftwo231
+
+func isPowerOfTwo(n int) bool {
+	if n <= 0 {
+		return false
+	}
+	for n > 0 {
+		m := n % 2
+		n = n / 2
+		if n > 0 && m != 0 {
+			return false
+		}
+	}
+	return true
+}
