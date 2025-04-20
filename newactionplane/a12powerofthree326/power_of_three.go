3cce8c8e6d7d3e4280d086efd790f18868b3ce59
mark <mark.h.tang@gmail.com>
2025-04-21 00:21:06 +0800

    feat: power-of-three version1

--- newactionplane/a12powerofthree326/power_of_three.go
+++ newactionplane/a12powerofthree326/power_of_three.go
@@ -1,5 +1,15 @@
 package a12powerofthree326
 
 func isPowerOfThree(n int) bool {
-	return true
+	for n > 0 {
+		if n == 1 {
+			return true
+		}
+		m := n % 3
+		if m != 0 {
+			return false
+		}
+		n /= 3
+	}
+	return false
 }
