cb21e65f8a22250a58145cfd922a63abd335a910
mark <mark.h.tang@gmail.com>
2025-04-24 00:39:23 +0800

    feat: shuffle_the_array version 1

--- newactionplane/a14shufflethearray/shuffle_the_array.go
+++ newactionplane/a14shufflethearray/shuffle_the_array.go
@@ -1,16 +1,12 @@
 package a14shufflethearray
 
-import "fmt"
-
 func shuffle(nums []int, n int) []int {
-	res := make([]int, 0, len(nums))
+	res := make([]int, len(nums))
 	for i := 0; i < n; i++ {
 		res[2*i] = nums[i]
 	}
-	fmt.Println(res)
-	// for i, j := 0, n; j < 2*n; i, j = i+1, j+1 {
-	// 	res[2*i+1] = nums[j]
-	// }
-
+	for i, j := 0, n; j < 2*n; i, j = i+1, j+1 {
+		res[2*i+1] = nums[j]
+	}
 	return res
 }
